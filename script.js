4065b93ce1fee87862e9dc4c1e7d215b2ecacc7c
Shadedog838 <32343626+Shadedog838@users.noreply.github.com>
2018-02-17 16:24:52 -0500

    intial save

---
+++ script.js
@@ -0,0 +1,23 @@
+window.alert('Save Net Neutrality');
+document.querySelector('#begin').addEventListener('click', function() {
+  var images=document.querySelectorAll("img")
+  document.querySelector("#women").src="https://media.giphy.com/media/sSgvbe1m3n93G/giphy.gif";
+  document.querySelector("#football").src="https://media.giphy.com/media/sSgvbe1m3n93G/giphy.gif";
+  document.querySelector("#pi").style.opacity=0.1;
+  document.querySelector("#bi").style.opacity=0.1;
+  document.querySelector("#begin").style.background="grey";
+
+  window.alert('Life is not fun without net-neutrality. Click the button below and mak a differnce');
+var links= document.querySelectorAll('a');
+  var numberLinks= links.length;
+for (var count=0; count<= links.length; count ++){
+  links[count].style.color="grey";
+
+}
+
+
+
+
+
+
+});
