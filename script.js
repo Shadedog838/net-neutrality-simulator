f11c91f8fbe1693cdf2cb629afef326edbbbb1fb
Shadedog838 <32343626+Shadedog838@users.noreply.github.com>
2018-02-24 00:48:32 -0500

    net neutrality

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.alert('Save Net Neutrality');
 document.querySelector('#begin').addEventListener('click', function() {
-  var images=document.querySelectorAll("img")
   document.querySelector("#women").src="https://media.giphy.com/media/sSgvbe1m3n93G/giphy.gif";
   document.querySelector("#football").src="https://media.giphy.com/media/sSgvbe1m3n93G/giphy.gif";
   document.querySelector("#pi").style.opacity=0.1;
@@ -10,8 +9,8 @@ document.querySelector('#begin').addEventListener('click', function() {
   window.alert('Life is not fun without net-neutrality. Click the button below and mak a differnce');
 var links= document.querySelectorAll('a');
   var numberLinks= links.length;
-for (var count=0; count<= links.length; count ++){
-  links[count].style.color="grey";
+for (var count=0; count< links.length; count ++){
+  links[count].href='';
 
 }
 
@@ -20,4 +19,26 @@ for (var count=0; count<= links.length; count ++){
 
 
 
+});
+
+  var numberSupport=0;
+
+document.querySelector('#submit').addEventListener('click', function() {
+var showNumber=document.querySelector('.Supporters');
+  var name= document.querySelector('#name').value;
+  var email= document.querySelector('#email').value;
+  var phonenumber= document.querySelector('#phonenumber').value;
+  var comment= document.querySelector('#comment').value;
+
+  if(name==''){
+window.alert('Please fill out name');
+}else if(email==''){
+  window.alert('Please fill out email');
+}else if(phonenumber==''){
+window.alert('please fill out phonenumber');
+}else{
+  window.alert('Thank you for your support');
+  numberSupport=numberSupport+1
+    showNumber.textContent=numberSupport;
+}
 });
